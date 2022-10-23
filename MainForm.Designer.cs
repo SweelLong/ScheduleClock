996edf5adfe1406fdb3807d4e3b657b35f8d9a9d
SweelLong <1485721541@qq.com>
2022-10-23 10:59:00 +0800

    push

---
+++ MainForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ScheduleClock
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Timer Monitor;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.Label0 = new System.Windows.Forms.Label();
+            this.Menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Item1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Item2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Icon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Item3 = new System.Windows.Forms.ToolStripMenuItem();
+            Monitor = new System.Windows.Forms.Timer(this.components);
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Monitor
+            // 
+            Monitor.Enabled = true;
+            Monitor.Interval = 1000;
+            Monitor.Tick += new System.EventHandler(this.Monitor_Tick);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Comic Sans MS", 110F);
+            this.Label1.ForeColor = System.Drawing.Color.LightCoral;
+            this.Label1.Location = new System.Drawing.Point(-29, 9);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(153, 206);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "1";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Comic Sans MS", 110F);
+            this.Label2.ForeColor = System.Drawing.Color.Lime;
+            this.Label2.Location = new System.Drawing.Point(74, 9);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(153, 206);
+            this.Label2.TabIndex = 1;
+            this.Label2.Text = "1";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Comic Sans MS", 110F);
+            this.Label3.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.Label3.Location = new System.Drawing.Point(268, 9);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(153, 206);
+            this.Label3.TabIndex = 1;
+            this.Label3.Text = "1";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Font = new System.Drawing.Font("Comic Sans MS", 110F);
+            this.Label4.ForeColor = System.Drawing.Color.Gold;
+            this.Label4.Location = new System.Drawing.Point(386, 9);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(153, 206);
+            this.Label4.TabIndex = 1;
+            this.Label4.Text = "1";
+            // 
+            // Label0
+            // 
+            this.Label0.AutoSize = true;
+            this.Label0.Font = new System.Drawing.Font("Comic Sans MS", 100F);
+            this.Label0.ForeColor = System.Drawing.Color.Aquamarine;
+            this.Label0.Location = new System.Drawing.Point(191, 9);
+            this.Label0.Name = "Label0";
+            this.Label0.Size = new System.Drawing.Size(118, 186);
+            this.Label0.TabIndex = 1;
+            this.Label0.Text = ":";
+            // 
+            // Menu
+            // 
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Item3,
+            this.Item1,
+            this.Item2});
+            this.Menu.Name = "contextMenuStrip1";
+            this.Menu.Size = new System.Drawing.Size(137, 70);
+            // 
+            // Item1
+            // 
+            this.Item1.Name = "Item1";
+            this.Item1.Size = new System.Drawing.Size(180, 22);
+            this.Item1.Text = "调整窗口";
+            this.Item1.Click += new System.EventHandler(this.Item1_Click);
+            // 
+            // Item2
+            // 
+            this.Item2.Name = "Item2";
+            this.Item2.Size = new System.Drawing.Size(180, 22);
+            this.Item2.Text = "退出";
+            this.Item2.Click += new System.EventHandler(this.Item2_Click);
+            // 
+            // Icon
+            // 
+            this.Icon.ContextMenuStrip = this.Menu;
+            this.Icon.Icon = ((System.Drawing.Icon)(resources.GetObject("Icon.Icon")));
+            this.Icon.Text = "ScheduleClock";
+            this.Icon.Visible = true;
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Font = new System.Drawing.Font("Comic Sans MS", 25F);
+            this.Label5.Location = new System.Drawing.Point(12, 205);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(0, 47);
+            this.Label5.TabIndex = 2;
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Font = new System.Drawing.Font("楷体", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label6.ForeColor = System.Drawing.Color.Black;
+            this.Label6.Location = new System.Drawing.Point(74, 220);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(0, 24);
+            this.Label6.TabIndex = 3;
+            // 
+            // Item3
+            // 
+            this.Item3.Name = "Item3";
+            this.Item3.Size = new System.Drawing.Size(180, 22);
+            this.Item3.Text = "移至正上方";
+            this.Item3.Click += new System.EventHandler(this.Item3_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 279);
+            this.Controls.Add(this.Label0);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Font = new System.Drawing.Font("楷体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ScheduleClock";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Label Label4;
+        private System.Windows.Forms.Label Label0;
+        private System.Windows.Forms.ContextMenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem Item1;
+        private System.Windows.Forms.NotifyIcon Icon;
+        private System.Windows.Forms.Label Label5;
+        private System.Windows.Forms.Label Label6;
+        private System.Windows.Forms.ToolStripMenuItem Item2;
+        private System.Windows.Forms.ToolStripMenuItem Item3;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,267 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ScheduleClock
+{
+    public partial class MainForm : Form
+    {
+        public MainForm() => InitializeComponent();
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists("SCconfig.json"))
+            {
+                FileExtract("ScheduleClock.SCconfig.json", AppDomain.CurrentDomain.BaseDirectory + "SCconfig.json");
+            }
+            Location = new Point(JsonReaderInt("上一次打开窗口的位置.X"), JsonReaderInt("上一次打开窗口的位置.Y"));
+            if (JsonReaderBool("隐藏窗口模式"))
+            {
+                FormBorderStyle = FormBorderStyle.None;
+                tag = false;
+            }
+            TopMost = true;
+            BackColor = Color.WhiteSmoke;
+            TransparencyKey = Color.WhiteSmoke;
+        }
+
+        private void Monitor_Tick(object sender, EventArgs e)
+        {
+            JsonWriter("上一次打开窗口的位置.X", Location.X);
+            JsonWriter("上一次打开窗口的位置.Y", Location.Y);
+            DateTime time = DateTime.Now;
+            string h = time.Hour.ToString();
+            string m = time.Minute.ToString();
+            if (h.Length != 2) h = "0" + h;
+            if (m.Length != 2) m = "0" + m;
+            Label1.Text = h.Substring(0, time.Hour.ToString().Length - 1);
+            Label2.Text = h.Substring(1, time.Hour.ToString().Length - 1);
+            Label3.Text = m.Substring(0, time.Hour.ToString().Length - 1);
+            Label4.Text = m.Substring(1, time.Hour.ToString().Length - 1);
+            Label5.Text = time.Second.ToString();
+            StringBuilder t = new StringBuilder();
+            t.Append(Label1.Text);
+            t.Append(Label2.Text);
+            t.Append(Label3.Text);
+            t.Append(Label4.Text);
+            if (JsonReaderStr(t.ToString()) != null)
+            {
+                Label6.Text = string.Format("{1}：{0}", JsonReaderStr(t.ToString()), JsonReaderStr("计划文本前的解释文字"));
+            }
+            else
+            {
+                int min = int.MaxValue;
+                string minValue = "?";
+                if (Label6.Text == "")
+                {
+                    string[] lines = File.ReadAllLines("SCconfig.json");
+                    foreach (string line in lines)
+                    {
+                        if (!line.Contains("\""))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            string[] core = line.Split(new string[] { "\"", "\"" }, StringSplitOptions.None);
+                            if (IsNumeric(core[1]))
+                            {
+                                if (int.Parse(t.ToString()) < int.Parse(core[1]))
+                                {
+                                    continue;
+                                }
+                                int D_value = Math.Abs(int.Parse(t.ToString()) - int.Parse(core[1]));
+                                if (D_value <= min)
+                                {
+                                    min = D_value;
+                                    minValue = core[1];
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                    }
+                    Label6.Text = string.Format("{1}：{0}", JsonReaderStr(minValue), JsonReaderStr("计划文本前的解释文字"));
+                }
+                //string f = File.ReadAllText("SCconfig.json");
+                //char[] chars = t.ToString().ToCharArray();
+                //int i = 0;
+                //if (chars[0].ToString() == "0")
+                //{
+                //    chars[0] = '9';
+                //    i = int.Parse(string.Concat(chars));
+                //    while (true)
+                //    {
+                //        int itemp = i - 9000;
+                //        if (f.Contains(itemp.ToString()))
+                //        {
+                //            Label6.Text = string.Format("{1}：{0}", JsonReaderStr(itemp.ToString()), JsonReaderStr("计划文本前的解释文字"));
+                //            break;
+                //        }
+                //        //if (i == 1260)
+                //        //{
+                //        //    break;
+                //        //}
+                //        char[] temp = new char[i];
+                //        if (temp[2] == '6')
+                //        {
+                //            temp[2] = '0';
+                //            temp[1] = (char)(int.Parse(temp[1].ToString()) + 1);
+                //        }
+                //        if (temp[1] == '6')
+                //        {
+                //            temp[1] = '0';
+                //            temp[0] = (char)(int.Parse(temp[1].ToString()) + 1);
+                //        }
+                //    }
+                //}
+                //else
+                //{
+                //    i = int.Parse(string.Concat(chars));
+                //    while (true)
+                //    {
+                //        if (f.Contains(i.ToString()))
+                //        {
+                //            Label6.Text = string.Format("{1}：{0}", JsonReaderStr(i.ToString()), JsonReaderStr("计划文本前的解释文字"));
+                //            break;
+                //        }
+                //        //if (i == 1260)
+                //        //{
+                //        //    break;
+                //        //}
+                //        char[] temp = new char[i];
+                //        if (temp[2] == '6')
+                //        {
+                //            temp[2] = '0';
+                //            temp[1] = (char)(int.Parse(temp[1].ToString()) + 1);
+                //        }
+                //        if (temp[1] == '6')
+                //        {
+                //            temp[1] = '0';
+                //            temp[0] = (char)(int.Parse(temp[1].ToString()) + 1);
+                //        }
+                //    }
+                //}
+            }
+        }
+
+        private static void FileExtract (string fn, string of)
+        {
+            BufferedStream bs = null;
+            FileStream fs = null;
+            try
+            {
+                Assembly a = Assembly.GetExecutingAssembly();
+                bs = new BufferedStream(a.GetManifestResourceStream(fn));
+                fs = new FileStream(of, FileMode.Create, FileAccess.Write);
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = bs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fs.Write(buffer, 0, length);
+                }
+                fs.Flush();
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Dispose();
+                }
+                if (bs != null)
+                {
+                    bs.Dispose();
+                }
+            }
+        }
+
+        private static int JsonReaderInt(string keywords)
+        {
+            StreamReader sr = File.OpenText("SCconfig.json");
+            JsonTextReader jtr = new JsonTextReader(sr);
+            JObject jo = (JObject)JToken.ReadFrom(jtr);
+            int value = (int)jo[keywords];
+            sr.Dispose();
+            return value;
+        }
+
+        private static string JsonReaderStr(string keywords)
+        {
+            StreamReader sr = File.OpenText("SCconfig.json");
+            JsonTextReader jtr = new JsonTextReader(sr);
+            JObject jo = (JObject)JToken.ReadFrom(jtr);
+            string value = (string)jo[keywords];
+            sr.Dispose();
+            return value;
+        }
+
+        private static bool JsonReaderBool(string keywords)
+        {
+            StreamReader sr = File.OpenText("SCconfig.json");
+            JsonTextReader jtr = new JsonTextReader(sr);
+            JObject jo = (JObject)JToken.ReadFrom(jtr);
+            bool value = (bool)jo[keywords];
+            sr.Dispose();
+            return value;
+        }
+
+        private static void JsonWriter(string keywords, dynamic newVaelue)
+        {
+            string str = File.ReadAllText("SCconfig.json");
+            dynamic jo = JsonConvert.DeserializeObject(str);
+            jo[keywords] = newVaelue;
+            File.WriteAllText("SCconfig.json", JsonConvert.SerializeObject(jo, Formatting.Indented));
+        }
+
+        private bool IsNumeric(string str)
+        {
+            if (str == null || str.Length == 0) 
+                return false;
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            byte[] bytestr = ascii.GetBytes(str);
+
+            foreach (byte c in bytestr)
+            {
+                if (c < 48 || c > 57)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool tag = true;
+
+        private void Item1_Click(object sender, EventArgs e)
+        {
+            if (tag == true)
+            {
+                FormBorderStyle = FormBorderStyle.None;
+                tag = false;
+            }
+            else
+            {
+                FormBorderStyle = FormBorderStyle.FixedSingle;
+                tag = true;
+            }
+        }
+
+        private void Item2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Item3_Click(object sender, EventArgs e)
+        {
+            Rectangle rect = Screen.GetWorkingArea(this);
+            Location = new Point(rect.Width / 3, 0);
+        }
+    }
+}
